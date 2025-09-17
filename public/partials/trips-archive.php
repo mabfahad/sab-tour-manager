0e35a3eac0446466b235035b8b9650e025d32b38
Fahad Abdullah <fahad.abdullah1280@gmail.com>
2025-09-17 10:44:41 +0600

    add image exception

--- public/partials/trips-archive.php
+++ public/partials/trips-archive.php
@@ -75,14 +75,23 @@ $query = new WP_Query($args);
                             <p class="trip-price">From SEK <?php echo esc_html($price);?></p>
                         </div>
 
-                        <div class="trip-featured-image">
-                            <?php if (has_post_thumbnail()) {
-                                the_post_thumbnail('medium_large', ['alt' => get_the_title()]);
-                            } ?>
-                        </div>
+                        <a href="<?php the_permalink(); ?>">
+                            <div class="trip-featured-image">
+                                <?php
+                                if (has_post_thumbnail()) {
+                                    the_post_thumbnail('medium_large', ['alt' => get_the_title()]);
+                                } else {
+                                    // Fallback image from plugin folder
+                                    $fallback_image = SAB_URL . '/img/fallbackimage.jpg';
+                                    echo '<img src="' . esc_url($fallback_image) . '" alt="' . esc_attr(get_the_title()) . '" />';
+                                }
+                                ?>
+                            </div>
+                        </a>
+
 
                         <div class="trip-content">
-                            <h3 class="trip-title"><?php the_title(); ?></h3>
+                            <a href="<?php the_permalink();?>"><h3 class="trip-title"><?php the_title(); ?></h3></a>
                             <p class="trip-description"><?php echo wp_trim_words(get_the_excerpt(), 20, '...'); ?></p>
                             <div class="readmore-wrapper">
                                 <a href="<?php the_permalink(); ?>" class="trip-read-more"><?php _e('Read more', 'sab-tour-manager'); ?></a>
